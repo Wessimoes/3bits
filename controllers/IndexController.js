d1c514cb16a4e89027b9ca04fab8ee740fc620ca
wesleyamaral <wesleyalopes2010@gmail.com>
2017-04-23 20:27:09 -0300

    Add and configuration nodemailer

--- controllers/IndexController.js
+++ controllers/IndexController.js
@@ -1,10 +1,43 @@
+ const config = require('../config/config');
+ const nodemailer = require('nodemailer');
+
  const indexController = {
 	// Route Index
 	index: (req, res, next) => {
 		res.render('index');
 	},
 	sendemail: (req, res, next) => {
-		res.send('send email here');
+
+		// Data about email
+		const name = req.body.name;
+		const email_sender = req.body.email;
+		const message = req.body.message;
+
+		// create transporter
+		let transporter = nodemailer.createTransport({
+			service: 'Gmail',
+			auth: {
+				user: config.email,
+				pass: config.senha
+			}
+		});
+
+		// setup email
+		let mailOptions = {
+			from: email_sender,
+			to: config.email,
+			subject: 'Teste',
+			html: message
+		};
+
+		// send email with defined transport object	
+		transporter.sendMail(mailOptions, (err, info) => {
+			if(err) {
+				res.send('erro ao enviar email');
+			}
+			res.redirect('back');
+		});
+
 	}
 }
 
